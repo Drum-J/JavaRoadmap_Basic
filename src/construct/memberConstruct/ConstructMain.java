1083047d7876b3ef0b2d5fea309b67c63857d877
승호 <tmdgh717@naver.com>
2024-12-18 14:10:20 +0900

    생성자 도입, 기본 생성자, 오버로딩과 this()

---
+++ src/construct/memberConstruct/ConstructMain.java
@@ -0,0 +1,15 @@
+package construct.memberConstruct;
+
+public class ConstructMain {
+    public static void main(String[] args) {
+        MemberConstruct member1 = new MemberConstruct("user1", 15, 90);
+        MemberConstruct member2 = new MemberConstruct("user2", 16, 80);
+        MemberConstruct member3 = new MemberConstruct("user2", 17);
+
+        MemberConstruct[] members = {member1, member2, member3};
+
+        for (MemberConstruct member : members) {
+            System.out.println("이름 : " + member.name + ", 나이 : " + member.age + ", 성적 : " + member.grade);
+        }
+    }
+}
---
+++ src/construct/memberDefault/DefaultMain.java
@@ -0,0 +1,7 @@
+package construct.memberDefault;
+
+public class DefaultMain {
+    public static void main(String[] args) {
+        MemberDefault memberDefault = new MemberDefault();
+    }
+}
