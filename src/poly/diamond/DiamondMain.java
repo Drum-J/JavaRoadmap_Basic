112716bc5798990f681089662ddc75727c7bbcb7
승호 <tmdgh717@naver.com>
2024-12-31 11:19:39 +0900

    인터페이스 - 다중 구현

---
+++ src/poly/diamond/DiamondMain.java
@@ -0,0 +1,20 @@
+package poly.diamond;
+/**
+ * 인터페이스 다중 구현
+ */
+public class DiamondMain {
+    public static void main(String[] args) {
+        InterfaceA a = new Child();
+        a.methodA();
+        a.methodCommon();
+
+        InterfaceB b = new Child();
+        b.methodB();
+        b.methodCommon();
+
+        Child child = new Child();
+        child.methodA();
+        child.methodB();
+        child.methodCommon();
+    }
+}
